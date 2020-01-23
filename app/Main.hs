1463915d8cfadcb3879ff52348c25f8a9e01e2fb
Christopher A. Upshaw <Christopher A. Upshaw>
2020-01-22 22:18:59 -0800

    Succesfully decoded to text!

--- app/Main.hs
+++ app/Main.hs
@@ -1,7 +1,35 @@
+{-# LANGUAGE BangPatterns #-}
+{-# LANGUAGE ScopedTypeVariables #-}
+{-# LANGUAGE OverloadedStrings #-}
+
+
 module Main (main) where
 
-import FactorioZygote (someFunc)
-
+import System.Environment (getArgs)
+import Data.ByteString.Base64 (decodeBase64)
+import Data.ByteString as BS (uncons)
+import Codec.Compression.Zlib as Zlib (decompress)
 
 main :: IO ()
-main = someFunc
+main = do
+
+  [path] <- getArgs
+  Just (n, blueprintString) <- BS.uncons <$> readFileBS path
+  putStrLn $ "read " <> path <> ":"
+  putStrLn $ "version number: " <> show n
+  putBSLn (blueprintString :: ByteString)
+
+  compressed <-
+    either
+      (die . toString)
+      (fmap toLazy . evaluateNF)
+      (decodeBase64 blueprintString)
+
+  putTextLn "Decoded base 64"
+
+  jasonByteString <- evaluateNF $ decompress compressed
+  putTextLn "Inflated with zlib:"
+  putTextLn . decodeUtf8 . toStrict $  jasonByteString
+
+  
+
